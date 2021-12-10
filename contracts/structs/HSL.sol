d73dbf8ea8a92db175720011bdca6436ff83633e
Utkarsh Sinha <sinha.karsh@gmail.com>
2021-12-10 15:15:07 -0800

    struct for HSL color

---
+++ contracts/structs/HSL.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title Struct representing HSL colors 
+ * @author Utkarsh Sinha <sinha.karsh@gmail.com>
+ */
+
+struct HSL {
+    uint16 hue;
+    uint8 saturation;
+    uint8 lightness;
+}
