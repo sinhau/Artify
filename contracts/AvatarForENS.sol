b81be53e79b920425a3872b114a1be7880bf60d3
Utkarsh Sinha <sinha.karsh@gmail.com>
2021-12-15 15:59:16 -0800

    formatting

--- contracts/AvatarForENS.sol
+++ contracts/AvatarForENS.sol
@@ -7,7 +7,6 @@ pragma solidity ^0.8.0;
  */
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "base64-sol/base64.sol";
